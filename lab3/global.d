76399b6c989e0d821298bfd6065e05c45463d79e
Cee <cee@chu2byo.com>
2014-08-04 01:23:12 +0800

    lab3

---
+++ lab3/global.d
@@ -0,0 +1,13 @@
+
+global.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 b8 0b 43 71 79 17 	movabs $0x3727a7177971430b,%rax
+   7:	a7 27 37 
+   a:	48 bd 00 bf ff ff ff 	movabs $0x7fffffffbf00,%rbp
+  11:	7f 00 00 
+  14:	68 f3 0e 40 00       	pushq  $0x400ef3
+  19:	c3                   	retq   
---
+++ lab3/global.s
@@ -0,0 +1,4 @@
+movabs $0x3727a7177971430b, %rax
+movabs $0x7fffffffbf00, %rbp
+pushq $0x400ef3
+retq
